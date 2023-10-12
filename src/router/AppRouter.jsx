1bdbff6af2af271366223911d484f874c4463bfc
kissmark <rivaskissmark@gmail.com>
2023-10-12 05:43:34 -0500

    add

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,17 +1,17 @@
 
 import {Navigate, Route,Routes} from 'react-router-dom'
-import { OnePiceMoviePages } from "../movies/pages/OnePiceMoviePages"
-import { PersonPages } from "../person/pages/PersonPages"
-import { CharactersPages } from '../characters/pages/CharactersPages'
+import { OnePiceMoviePages } from "../OnePiceMoviePages"
+import { PersonPages } from "../PersonPages"
+import { CharactersPages } from '../CharactersPages'
 
 export const AppRouter = () => {
   return (
     <>
     <Routes>
-      <Route path='movies' element ={<OnePiceMoviePages/>}/>
+      <Route path='/' element ={<OnePiceMoviePages/>}/>
       <Route path='characters/:mal_id' element ={<CharactersPages/>} />
       <Route path='person/:mal_id' element ={<PersonPages/>} />
-      <Route path='/' element ={<Navigate to="/movies"/>} />
+      {/* <Route path='/' element ={<Navigate to="/movies"/>} /> */}
     </Routes>
     </>
   )
